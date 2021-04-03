69b9972c638c154b87ae765dba821c903d3eb9f8
MaroderKo <maroderko8@gmail.com>
2021-04-03 10:11:03 +0300

    HW3

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -7,27 +7,63 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage{
     @Override
     public void clear() {
-
+        Arrays.fill(storage,0,size,null);
+        size = 0;
     }
 
     @Override
     public void update(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        storage[index] = r;
     }
 
     @Override
     public void save(Resume r) {
+        int index = -1;
+        for (int i = 0; i < size; i++)
+        {
+            if (r.compareTo(storage[i]) < 0)
+            {
+                index = i;
+                break;
+            }
+        }
+        if(index == -1)
+        {
+            storage[size] = r;
 
+        }
+        else
+        {
+
+            Resume buf = r, buf_new;
+            for (int i = index; i<size+1;i++)
+            {
+                buf_new = storage[i];
+                storage[i] = buf;
+                buf = buf_new;
+
+            }
+        }
+        size++;
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index = getIndex(uuid);
+        if (index != -1)
+        {
+            for (int i = index; i<size; i++)
+            {
+                storage[i] = storage[i+1];
+            }
+            size--;
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage,0,size);
     }
 
     @Override
